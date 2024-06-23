1aa2244bfaa266a7096c50f1124c63b2217742e4
martinfyic <martin.f.yic@gmail.com>
2024-06-22 21:06:53 -0300

    refactor: :fire: remove db envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,9 +3,6 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  POSTGRES_USER: string;
-  POSTGRES_PASSWORD: string;
-  POSTGRES_DB: string;
   DATABASE_URL: string;
   NATS_SERVERS: string[];
 }
@@ -13,9 +10,6 @@ interface EnvVars {
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    POSTGRES_USER: joi.string().required(),
-    POSTGRES_PASSWORD: joi.string().required(),
-    POSTGRES_DB: joi.string().required(),
     DATABASE_URL: joi.string().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
@@ -34,9 +28,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  postgresUser: envVars.POSTGRES_USER,
-  postgresPassword: envVars.POSTGRES_PASSWORD,
-  postgresDb: envVars.POSTGRES_DB,
   databaseUrl: envVars.DATABASE_URL,
   natsServers: envVars.NATS_SERVERS,
 };
